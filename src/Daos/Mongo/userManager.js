a0f16240b367cfa6a8b5dc41e4539ef23de0246c
AliceProjectVT <islamartinezd@gmail.com>
2023-10-27 19:48:43 -0300

    Corrección  createUsers a createUser para función de creacion de usuario en userManager y Sessions

--- src/Daos/Mongo/userManager.js
+++ src/Daos/Mongo/userManager.js
@@ -1,28 +1,28 @@
 import userModel from "./models/user.model.js";
 
-class userManagerMongo{
-    constructor(){
-        this.model=userModel
+class userManagerMongo {
+    constructor() {
+        this.model = userModel
     }
 
 
-    async getUsers(){
-        try{
+    async getUsers() {
+        try {
             return await this.model.find({})
 
-        }catch(error){
+        } catch (error) {
             console.log(error)
         }
     }
-    async getUser(uid){
-                    return await this.model.findOne({_id: uid})
-          }
-    async createUser(newUser){
+    async getUser(filter) {
+        return await this.model.findOne({ filter })
+    }
+    async createUser(newUser) {
         return await this.model.create(newUser)
 
     }
-    async updateUser(){}
-    async deleteUser(){}
+    async updateUser() { }
+    async deleteUser() { }
 }
 
 export default userManagerMongo
--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -9,8 +9,10 @@ const initializePassport = () => {
         clientSecret: '4f3ab9c7741a5ae2eb340c6776744a6af8f25e56',
         callbackURL: 'http://localhost:4000/api/sessions/githubcallback'
     }, async (accesToken, refreshToken, profile, done) => {
+        console.log('profile:', profile)
         try {
             let user = await userService.getUser({ email: profile._json.email })
+
             if (!user) {
                 let newUser = {
                     first_name: profile.username,
@@ -18,7 +20,7 @@ const initializePassport = () => {
                     email: profile._json.email,
                     password: ''
                 }
-                let result = await userService.createUsers(newUser)
+                let result = await userService.createUser(newUser)
                 return done(null, result)
             }
             return done(null, user)
@@ -31,7 +33,7 @@ const initializePassport = () => {
     passport.serializeUser((user, done) => {
         done(null, user._id)
     })
-    passport.deserializeUser(async () => {
+    passport.deserializeUser(async (id, done) => {
         let user = await userService.getUser({ _id: id })
         done(null, user)
     })
