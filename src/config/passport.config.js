79a0474adc9e0f5db04100b88054bbc47279f856
AliceProjectVT <islamartinezd@gmail.com>
2023-10-27 16:09:59 -0300

    fix dirección para callback, se añade el puerto 4000

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -2,37 +2,37 @@ import passport from "passport"
 import GithubStrategy from "passport-github2"
 import userManagerMongo from "../Daos/Mongo/userManager.js"
 
-
+const userService = new userManagerMongo
 const initializePassport = () => {
-passport.use('github', new GithubStrategy({
-    clientID:'Iv1.53504297a2288e65',
-    clientSecret: '4f3ab9c7741a5ae2eb340c6776744a6af8f25e56',
-    callbackURL:'http://localhost/api/sessions/githubcallback'
-}, async (accesToken, refreshToken, profile, done)=>{
-try{
-    let user= await userService.getUser({email: profile._json.email})
-    if(!user){
-        let newUser = {
-            first_name: profile.username,
-            last_name: profile.username,
-            email: profile._json.email,
-            password:''
+    passport.use('github', new GithubStrategy({
+        clientID: 'Iv1.53504297a2288e65',
+        clientSecret: '4f3ab9c7741a5ae2eb340c6776744a6af8f25e56',
+        callbackURL: 'http://localhost:4000/api/sessions/githubcallback'
+    }, async (accesToken, refreshToken, profile, done) => {
+        try {
+            let user = await userService.getUser({ email: profile._json.email })
+            if (!user) {
+                let newUser = {
+                    first_name: profile.username,
+                    last_name: profile.username,
+                    email: profile._json.email,
+                    password: ''
+                }
+                let result = await userService.createUsers(newUser)
+                return done(null, result)
+            }
+            return done(null, user)
+        } catch (error) {
+            return done(error)
         }
-        let result = await userService.createUsers(newUser)
-        return done(null, result)
-    }
-return done(null, user)
-}catch(error){
-    return done(error)
-}
-    
-} ) )
 
-passport.serializeUser((user,done)=>{
-    done(null,user._id)
-})
-    passport.deserializeUser(async()=>{
-        let user = await userService.getUser({_id: id})
+    }))
+
+    passport.serializeUser((user, done) => {
+        done(null, user._id)
+    })
+    passport.deserializeUser(async () => {
+        let user = await userService.getUser({ _id: id })
         done(null, user)
     })
 
